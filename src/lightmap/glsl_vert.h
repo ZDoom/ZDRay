1d51bc17de03ca3402e0790ad60d49b570bde745
Magnus Norddahl <dpjudas@users.noreply.github.com>
2022-07-03 02:01:01 +0200

    Fix triangle strip not covering the entire tile

--- src/lightmap/glsl_vert.h
+++ src/lightmap/glsl_vert.h
@@ -22,9 +22,9 @@ layout(location = 0) out vec3 worldpos;
 
 vec2 positions[4] = vec2[](
 	vec2(0.0, 0.0),
+	vec2(1.0, 0.0),
 	vec2(0.0, 1.0),
-	vec2(1.0, 1.0),
-	vec2(1.0, 0.0)
+	vec2(1.0, 1.0)
 );
 
 void main()
